7e522add2eb925e1d5e21d8406ac6c07df735bc5
hultdin <34416082+hultdin@users.noreply.github.com>
2021-09-29 13:53:57 +0200

    Update passivedns.sql

--- passivedns.sql
+++ passivedns.sql
@@ -12,21 +12,3 @@ SELECT MIN(time), name, ttl, class, type, data FROM answers GROUP BY name, ttl, 
 
 UPDATE answers SET name = RTRIM(name, '.') WHERE name LIKE '%.';
 UPDATE answers SET data = RTRIM(data, '.') WHERE type = "CNAME" AND data LIKE '%.';
-
-
-SELECT * FROM answers a WHERE a.name = 'www.tm.a.prd.aadg.akadns.net' AND a.time != (SELECT MIN(time) FROM answers b WHERE a.name = b.name AND a.ttl = b.ttl AND a.class = b.class AND a.type = b.type AND a.data = b.data) GROUP BY name;
-
-SELECT * FROM answers a WHERE a.name = 'www.swedbank.se' AND a.time NOT IN (SELECT MIN(time) FROM answers b WHERE a.name = b.name AND a.ttl = b.ttl AND a.class = b.class AND a.type = b.type AND a.data = b.data);
-
-
-DELETE FROM answers a WHERE a.name = 'www.tm.a.prd.aadg.akadns.net' AND a.time != (SELECT MIN(time) FROM answers b WHERE a.name = b.name AND a.ttl = b.ttl AND a.class = b.class AND a.type = b.type AND a.data = b.data) GROUP BY name;
-
-DELETE FROM answers a WHERE a.time NOT IN (SELECT MIN(time) FROM answers b WHERE a.name = b.name AND a.ttl = b.ttl AND a.class = b.class AND a.type = b.type AND a.data = b.data);
-
-
-go build -ldflags "-s -w"
-
-
-
-SELECT * FROM answers WHERE answers.name = 'www.swedbank.se' AND answers.time NOT IN (SELECT MIN(time) FROM answers a WHERE answers.name = a.name AND answers.ttl = a.ttl AND answers.class = a.class AND answers.type = a.type AND answers.data = a.data);
-DELETE FROM answers WHERE answers.name = 'www.swedbank.se' AND answers.time NOT IN (SELECT MIN(time) FROM answers a WHERE answers.name = a.name AND answers.ttl = a.ttl AND answers.class = a.class AND answers.type = a.type AND answers.data = a.data);
